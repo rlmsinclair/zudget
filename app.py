50f8957a59bb5518cc19c94d09ccc9ded62a24fb
Robert Sinclair <44088355+rlmsinclair@users.noreply.github.com>
2024-02-17 13:47:05 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, session, redirect, request, url_for, flash
+import csv
+import flask_login
+from flask_login import UserMixin, LoginManager, login_user
+from flask_sqlalchemy import SQLAlchemy
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from wtforms import StringField , PasswordField , SubmitField
+from flask_bcrypt import Bcrypt
+from flask_wtf import FlaskForm
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://doadmin:AVNS_Ed7FVk0dyVGlhXuZmb4@db-postgresql-lon1-41546-do-user-14798294-0.c.db.ondigitalocean.com:25060/defaultdb?sslmode=require'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+bcrypt = Bcrypt(app)
+app.config['SECRET_KEY'] = '1234'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    with app.app_context():
+        return User.query.get(int(user_id))
+    
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+
+# Registration Form
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(min=5, max=50)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=50)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        if User.query.filter_by(email=email).first():  # the query has returned a user
+            flash("Email already in use, please log in or use a different email.")
+            return redirect (url_for('register'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_user = User(email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration Successful !')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        is_valid = bcrypt.check_password_hash(user.password, password)
+        if user and is_valid and user.is_confirmed:
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('login failed , check your username and password', 'danger')
+    return render_template('login.html')
+
+
+@app.route('/')
+def home():
+    user = flask_login.current_user
+    if user.is_anonymous:
+        return redirect(url_for('login'))
+    return render_template('index.html')
+
+
+@app.route('/save')
+def save():
+    pass
+
+if __name__ == '__main__':
+    
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(debug=True)
+
+
